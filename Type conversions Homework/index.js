644e172616f77655bec0de978269e9349a6de695
Kire Zlatkovski <kire.zlatkovski@live.com>
2022-07-29 01:26:30 +0200

    domasno

---
+++ Type conversions Homework/index.js
@@ -0,0 +1,51 @@
+let numOne = 1 ; let stringNumOne = String(numOne) ; let booleanNumOne = Boolean(numOne);
+console.log(typeof numOne , typeof stringNumOne, typeof booleanNumOne)
+
+let numZero = 0 ; let stringNumZero = String(numZero); let booleanNumZero = Boolean(numZero)
+console.log(typeof numZero , typeof stringNumZero, typeof booleanNumZero)
+
+let stringOne = "1" ; let numStringOne = Number(stringOne) ; let booleanStringOne = Boolean(stringOne);
+console.log(typeof stringOne , typeof numStringOne , typeof booleanStringOne )
+
+let stringZero = "0" ; let numStringZero = Number(stringZero) ; let booleanStringZero = Boolean(stringZero);
+console.log(typeof stringZero , typeof numStringZero , typeof booleanStringZero )
+
+let stringTen = "ten" ; let numStringTen = Number(stringTen) ; let booleanStringTen = Boolean(stringTen);
+console.log(typeof stringTen , typeof numStringTen , typeof booleanStringTen )
+
+let booleanTrue = true ; let stringBooleanTrue = String(booleanTrue) ; let numBooleanTrue = Number(booleanTrue);
+console.log (typeof booleanTrue , typeof stringBooleanTrue ,typeof numBooleanTrue)
+
+let booleanFalse = true ; let stringBooleanFalse = String(booleanFalse) ; let numBooleanFalse = Number(booleanFalse);
+console.log (typeof booleanFalse , typeof stringBooleanFalse ,typeof numBooleanFalse)
+
+let nulll = null ; let booleanNulll = Boolean(nulll) ; let numNulll = Number(nulll)
+console.log(typeof nulll, typeof booleanNulll , typeof numNulll)
+
+let undefine = undefined ; let booleanUndefine = Boolean(undefine) ; let numundefine = Number(undefine)
+console.log(typeof undefine, typeof booleanUndefine , typeof numundefine)
+
+let stringDoubleEmpty = "" ; let numStringDoubleEmpty = Number(stringDoubleEmpty) ; let booleanStringDoubleEmpty = Boolean(stringDoubleEmpty);
+console.log(typeof stringDoubleEmpty , typeof numStringDoubleEmpty , typeof booleanStringDoubleEmpty )
+
+let stringEmpty = '' ; let numStringEmpty = Number(stringEmpty) ; let booleanStringEmpty = Boolean(stringEmpty);
+console.log(typeof stringEmpty , typeof numStringEmpty , typeof booleanStringEmpty )
+
+// Second
+let first = 15 ;
+let second = 14 ;
+let third = 95-19 ;
+let fourth = 1+9+9+5 ;
+console.log( first , second , third , fourth)
+
+// Third
+let fifth = true ? first>second :fifth = false
+console.log (fifth)
+let sixth = (third % fourth);
+console.log(sixth)
+let seventh = "I am the"
+if (fifth = true && sixth < 3) {
+    console.log(seventh + " light");
+}else {
+    console.log(seventh + " darkness");
+}
